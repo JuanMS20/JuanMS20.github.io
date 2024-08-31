ac05fe5a95209680ea03ee0ad7b84c6d0c9eb95e
JuanMS20 <cojuanv1@gmail.com>
2024-08-30 21:40:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,107 +1,153 @@
-// Se adiciona un "event listener" para cuando se cargue todo el contenido de la página:
-document.addEventListener("DOMContentLoaded", () => { 
+document.addEventListener("DOMContentLoaded", () => {
+    // ================================
+    // Funcionalidad para la Información Personal
+    // ================================
 
-    // ================================
-    // Funcionalidad para la lista de tareas
-    // ================================
-    
-    // Se obtiene referencia a la lista dinámica:
-    const taskList = document.getElementById("taskList");
-    
-    // Referencia al elemento seleccionado de la lista:
-    let selectedTask = null;
-    
-    // Se adiciona event listener a cada botón:
-    document.getElementById("addTask").addEventListener("click", () => {
-        const textoTarea = prompt("Ingrese el nombre de la tarea");
-        if (textoTarea) {
-            const nuevaTarea = document.createElement("li");
-            nuevaTarea.textContent = textoTarea;
-            taskList.appendChild(nuevaTarea);
-            selectedTaskHandler(nuevaTarea);
+    const personalInfoList = document.getElementById("personalInfoList");
+    let selectedPersonalInfo = null;
+
+    document.getElementById("addPersonalInfo").addEventListener("click", () => {
+        const nuevaInfo = prompt("Ingrese nueva información personal");
+        if (nuevaInfo) {
+            const nuevaElemento = document.createElement("li");
+            nuevaElemento.textContent = nuevaInfo;
+            nuevaElemento.classList.add("list-group-item");
+            personalInfoList.appendChild(nuevaElemento);
+            selectPersonalInfoHandler(nuevaElemento);
         }
     });
-    
-    document.getElementById("deleteTask").addEventListener("click", () => {
-        if (selectedTask) {
-            taskList.removeChild(selectedTask);
-            selectedTask = null;
-        } else {
-            alert("No ha seleccionado ninguna tarea para eliminar");
-        }
-    });
-    
-    document.getElementById("editTask").addEventListener("click", () => {
-        if (selectedTask) {
-            const nuevoTexto = prompt("Modificar la tarea", selectedTask.textContent);
-            if (nuevoTexto) {
-                selectedTask.textContent = nuevoTexto;
+
+    document.getElementById("editPersonalInfo").addEventListener("click", () => {
+        if (selectedPersonalInfo) {
+            const nuevaInfo = prompt("Modificar la información", selectedPersonalInfo.textContent);
+            if (nuevaInfo) {
+                selectedPersonalInfo.textContent = nuevaInfo;
             }
         } else {
-            alert("Seleccione una tarea para modificar");
+            alert("Seleccione una información para editar");
         }
     });
-    
-    function selectedTaskHandler(task) {
-        task.addEventListener("click", () => {
-            if (selectedTask) {
-                selectedTask.style.backgroundColor = ""; // Quita el estilo de la tarea previamente seleccionada
+
+    document.getElementById("deletePersonalInfo").addEventListener("click", () => {
+        if (selectedPersonalInfo) {
+            personalInfoList.removeChild(selectedPersonalInfo);
+            selectedPersonalInfo = null;
+        } else {
+            alert("No ha seleccionado ninguna información para eliminar");
+        }
+    });
+
+    function selectPersonalInfoHandler(info) {
+        info.addEventListener("click", () => {
+            if (selectedPersonalInfo) {
+                selectedPersonalInfo.classList.remove("active");
             }
-            selectedTask = task;
-            selectedTask.style.backgroundColor = "#f0f0f0"; // Resalta la tarea seleccionada
+            selectedPersonalInfo = info;
+            selectedPersonalInfo.classList.add("active");
         });
     }
 
+    document.querySelectorAll("#personalInfoList li").forEach(selectPersonalInfoHandler);
+
+
     // ================================
-    // Funcionalidad para la lista de skills
+    // Funcionalidad para la Educación
     // ================================
-    
-    // Se obtiene referencia a la lista de skills:
-    const skillsList = document.getElementById("skillsList");
-    
-    // Referencia al skill seleccionado:
-    let selectedSkill = null;
-    
-    // Event listener para agregar un skill:
-    document.getElementById("addSkill").addEventListener("click", () => {
-        const textoSkill = prompt("Ingrese el nombre del skill");
-        if (textoSkill) {
-            const nuevoSkill = document.createElement("li");
-            nuevoSkill.textContent = textoSkill;
-            skillsList.appendChild(nuevoSkill);
-            selectedSkillHandler(nuevoSkill);
+
+    const educationInfoList = document.getElementById("educationInfoList");
+    let selectedEducationInfo = null;
+
+    document.getElementById("addEducationInfo").addEventListener("click", () => {
+        const nuevaEducacion = prompt("Ingrese nueva información educativa");
+        if (nuevaEducacion) {
+            const nuevaElemento = document.createElement("li");
+            nuevaElemento.textContent = nuevaEducacion;
+            nuevaElemento.classList.add("list-group-item");
+            educationInfoList.appendChild(nuevaElemento);
+            selectEducationInfoHandler(nuevaElemento);
         }
     });
 
-    // Event listener para borrar un skill:
-    document.getElementById("deleteSkill").addEventListener("click", () => {
-        if (selectedSkill) {
-            skillsList.removeChild(selectedSkill);
-            selectedSkill = null;
-        } else {
-            alert("No ha seleccionado ningún skill para eliminar");
-        }
-    });
-
-    // Event listener para editar un skill:
-    document.getElementById("editSkill").addEventListener("click", () => {
-        if (selectedSkill) {
-            const nuevoTexto = prompt("Modificar el skill", selectedSkill.textContent);
-            if (nuevoTexto) {
-                selectedSkill.textContent = nuevoTexto;
+    document.getElementById("editEducationInfo").addEventListener("click", () => {
+        if (selectedEducationInfo) {
+            const nuevaInfo = prompt("Modificar la educación", selectedEducationInfo.textContent);
+            if (nuevaInfo) {
+                selectedEducationInfo.textContent = nuevaInfo;
             }
         } else {
-            alert("Seleccione un skill para modificar");
+            alert("Seleccione una educación para editar");
         }
     });
 
-    function selectedSkillHandler(skill) {
-        skill.addEventListener("click", () => {
-            if (selectedSkill) {
-                selectedSkill.style.backgroundColor = ""; // Quita el estilo del skill previamente seleccionado
+    document.getElementById("deleteEducationInfo").addEventListener("click", () => {
+        if (selectedEducationInfo) {
+            educationInfoList.removeChild(selectedEducationInfo);
+            selectedEducationInfo = null;
+        } else {
+            alert("No ha seleccionado ninguna educación para eliminar");
+        }
+    });
+
+    function selectEducationInfoHandler(info) {
+        info.addEventListener("click", () => {
+            if (selectedEducationInfo) {
+                selectedEducationInfo.classList.remove("active");
             }
-            selectedSkill = skill;
-            selectedSkill.style.backgroundColor = "#f0f0f0"; // Resalta el skill seleccionado
+            selectedEducationInfo = info;
+            selectedEducationInfo.classList.add("active");
         });
     }
+
+    document.querySelectorAll("#educationInfoList li").forEach(selectEducationInfoHandler);
+
+
+    // ================================
+    // Funcionalidad para la Experiencia Laboral
+    // ================================
+
+    const workExperienceList = document.getElementById("workExperienceList");
+    let selectedWorkExperience = null;
+
+    document.getElementById("addWorkExperience").addEventListener("click", () => {
+        const nuevaExperiencia = prompt("Ingrese nueva experiencia laboral");
+        if (nuevaExperiencia) {
+            const nuevaElemento = document.createElement("li");
+            nuevaElemento.textContent = nuevaExperiencia;
+            nuevaElemento.classList.add("list-group-item");
+            workExperienceList.appendChild(nuevaElemento);
+            selectWorkExperienceHandler(nuevaElemento);
+        }
+    });
+
+    document.getElementById("editWorkExperience").addEventListener("click", () => {
+        if (selectedWorkExperience) {
+            const nuevaInfo = prompt("Modificar la experiencia laboral", selectedWorkExperience.textContent);
+            if (nuevaInfo) {
+                selectedWorkExperience.textContent = nuevaInfo;
+            }
+        } else {
+            alert("Seleccione una experiencia laboral para editar");
+        }
+    });
+
+    document.getElementById("deleteWorkExperience").addEventListener("click", () => {
+        if (selectedWorkExperience) {
+            workExperienceList.removeChild(selectedWorkExperience);
+            selectedWorkExperience = null;
+        } else {
+            alert("No ha seleccionado ninguna experiencia laboral para eliminar");
+        }
+    });
+
+    function selectWorkExperienceHandler(info) {
+        info.addEventListener("click", () => {
+            if (selectedWorkExperience) {
+                selectedWorkExperience.classList.remove("active");
+            }
+            selectedWorkExperience = info;
+            selectedWorkExperience.classList.add("active");
+        });
+    }
+
+    document.querySelectorAll("#workExperienceList li").forEach(selectWorkExperienceHandler);
 });
